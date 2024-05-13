83e4d7506fc6ec851d65b80b39ac76a7a4d65f60
Lloyd Boone <lboone@progressive.com>
2024-05-13 19:44:26 -0400

    implement shadcn/ui and toast by sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ConvexClientProvider from "./ConvexClientProvider";
+import { Toaster } from "sonner";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Toaster />
+          {children}</ConvexClientProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { api } from "../../convex/_generated/api";
 import { useEffect } from "react";
 import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function Home() {
   const storeUser = useMutation(api.users.store);
@@ -14,9 +16,10 @@ export default function Home() {
   });
 
   return (
-    <>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
-      <SignOutButton />
-    </>
+      <Button variant="secondary" onClick={()=> {toast.success("It was successful")}}>Click Me</Button>
+      <Button variant="outline"><SignOutButton /></Button>
+    </div>
   );
 }
